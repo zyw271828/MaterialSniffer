ea798d92d97b4f674bcf69520bde0251dd169779
zyw271828 <zyw_0618@outlook.com>
2018-12-01 13:03:07 +0800

    Add packet information dialog

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -2,17 +2,21 @@ package application;
 
 import java.io.EOFException;
 import java.net.Inet4Address;
+import java.util.HashMap;
 import java.util.concurrent.TimeoutException;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 import org.pcap4j.core.NotOpenException;
 import org.pcap4j.core.PcapHandle;
@@ -70,15 +74,14 @@ public class Controller {
                 Platform.runLater(new Runnable() {
                     public void run() {
                         JFXButton button = new JFXButton(protocol.name());
-                        button.setAlignment(Pos.CENTER);
                         button.setPrefWidth(length / 2);
                         button.setTextFill(Color.WHITE);
                         button.setStyle("-fx-font: 14 arial;");
 
-                        Color color = Color.WHITE;
+                        Color color;
                         switch (button.getText()) {
                         case "TCP":
-                            color = Color.web("#2196F3");
+                            color = Color.web("#0091EA");
                             break;
                         case "UDP":
                             color = Color.web("#199B18");
@@ -87,11 +90,37 @@ public class Controller {
                             color = Color.web("#E51C17");
                             break;
                         default:
+                            color = Color.web("#9E9E9E");
                             break;
                         }
 
                         button.setBackground(
                                 new Background(new BackgroundFill(color, CornerRadii.EMPTY, Insets.EMPTY)));
+
+                        HashMap<JFXButton, IpV4Packet> map = new HashMap<>();
+                        map.put(button, ipV4Packet);
+
+                        button.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
+                            AnchorPane root = new AnchorPane();
+                            IpV4Packet pkt = map.get(button);
+                            String info = getPacketInfo(pkt);
+
+                            Stage stage = new Stage();
+                            stage.setResizable(false);
+                            stage.setAlwaysOnTop(true);
+                            stage.setTitle(pkt.getHeader().getProtocol().name() + " 详情");
+                            stage.setScene(new Scene(root, 600, 400));
+                            JFXTextArea infoArea = new JFXTextArea();
+                            infoArea.setLayoutX(20);
+                            infoArea.setLayoutY(20);
+                            infoArea.setPrefWidth(560);
+                            infoArea.setPrefHeight(360);
+                            infoArea.setStyle("-fx-font: 18 'Droid Sans Mono for Powerline';");
+                            infoArea.setText(info);
+                            root.getChildren().add(infoArea);
+                            stage.show();
+                        });
+
                         displayMasonryPane.getChildren().add(button);
                     }
                 });
@@ -104,5 +133,26 @@ public class Controller {
         } catch (NotOpenException e) {
         }
         return true;
+    }
+
+    private String getPacketInfo(IpV4Packet pkt) {
+        String info = "";
+        info = info + "版本: " + pkt.getHeader().getVersion() + "\n";
+        info = info + "IHL: " + pkt.getHeader().getIhl() + "\n";
+        info = info + "区分服务: " + pkt.getHeader().getTos() + "\n";
+        info = info + "总长度: " + pkt.getHeader().getTotalLength() + "\n";
+        info = info + "标识: " + pkt.getHeader().getIdentification() + "\n";
+        info = info + "不分段: " + pkt.getHeader().getDontFragmentFlag() + "\n";
+        info = info + "更多分段: " + pkt.getHeader().getMoreFragmentFlag() + "\n";
+        info = info + "分段偏移量: " + pkt.getHeader().getFragmentOffset() + "\n";
+        info = info + "TTL: " + pkt.getHeader().getTtl() + "\n";
+        info = info + "协议: " + pkt.getHeader().getProtocol() + "\n";
+        info = info + "头校验和: " + pkt.getHeader().getHeaderChecksum() + "\n";
+        info = info + "源地址: " + pkt.getHeader().getSrcAddr().getHostAddress() + "\n";
+        info = info + "目标地址: " + pkt.getHeader().getDstAddr().getHostAddress() + "\n";
+        info = info + "选项: " + pkt.getHeader().getOptions() + "\n";
+        info = info + "\n";
+        info = info + "负载: \n" + pkt.getPayload() + "\n";
+        return info;
     }
 }
